e28dc3555a81a6df9fbd5c823873f6d796d7f096
Sviatlana <sveta_04071987@mail.ru>
2022-10-16 15:43:54 +0300

    add:init js file

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,40 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  newStr = [];
+  rep = options.repeatTimes || 1;
+
+  for (let i = 1; i <= rep; i++) {
+    let add = [];
+    if (options.additionRepeatTimes) {
+      for (let j = 1; j <= options.additionRepeatTimes; j++) {
+        add.push(String(options.addition));
+      }
+    }
+    if (options.addition && !options.additionRepeatTimes) {
+      add.push(String(options.addition));
+    }
+
+    newStr.push(str + add.join(options.additionSeparator || "|"));
+  }
+  return newStr.join(options.separator || "+");
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,87 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+
+    let strArr = str.toUpperCase().split("");
+    let keyArr =
+      str.length > key.length
+        ? key
+            .repeat(Math.ceil(str.length / key.length))
+            .toUpperCase()
+            .split("")
+        : key.toUpperCase().split("");
+
+    let newStr = "";
+
+    strArr.forEach((s) => {
+      if (s.charCodeAt() > 64 && s.charCodeAt() < 91) {
+        let sCode = s.charCodeAt() - 65;
+        let kCode = keyArr.splice(0, 1).join("").charCodeAt() - 65;
+        let nKey = ((sCode + kCode) % 26) + 65;
+        newStr += String.fromCharCode(nKey);
+      } else {
+        newStr += s;
+      }
+    });
+
+    return this.reverse ? newStr : newStr.split("").reverse().join("");
+  }
+  decrypt(str, key) {
+    if (!str || !key) throw new Error("Incorrect arguments!");
+
+    let strArr = str.toUpperCase().split("");
+    let keyArr =
+      str.length > key.length
+        ? key
+            .repeat(Math.ceil(str.length / key.length))
+            .toUpperCase()
+            .split("")
+        : key.toUpperCase().split("");
+
+    let newStr = "";
+
+    strArr.forEach((s) => {
+      if (s.charCodeAt() > 64 && s.charCodeAt() < 91) {
+        let sCode = s.charCodeAt() - 65;
+        let kCode = keyArr.splice(0, 1).join("").charCodeAt() - 65;
+        let nKey =
+          sCode - kCode < 0
+            ? 91 + (sCode - kCode)
+            : ((sCode - kCode) % 26) + 65;
+        newStr += String.fromCharCode(nKey);
+      } else {
+        newStr += s;
+      }
+    });
+
+    return this.reverse ? newStr : newStr.split("").reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
